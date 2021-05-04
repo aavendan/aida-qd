d06b64c67cd3206a5fb63fedc194d8229fcd83b5
aavendan <aavendan@gmail.com>
2021-05-04 12:16:16 -0500

    04/05 1 vs All - bar instead lines

--- src/app/views/theme/typography.component.ts
+++ src/app/views/theme/typography.component.ts
@@ -18,12 +18,12 @@ export class TypographyComponent implements OnInit {
   // barChart1
   public barChart1Data: Array<any> = [
     {
-      data: [60, 58, 82, 70,72],
+      data: [60,72, 58, 82, 70],
       label: 'PAT',
       barPercentage: 0.70,
     }
   ];
-  public barChart1Labels: Array<any> = ['Anvers','Lille', 'Brugge','Barcelona', 'Paris'];
+  public barChart1Labels: Array<any> = ['Anvers', 'Paris','Lille', 'Brugge','Barcelona'];
   public barChart1Options: any = {
     /* tooltips: {
       enabled: false,
@@ -72,19 +72,34 @@ export class TypographyComponent implements OnInit {
   public barChart1Legend = false;
   public barChart1Type = 'bar';
 
-  // barChar2
+  // barChart2
   public barChart2Data: Array<any> = [
     {
-      data: [0, 60, 0, 1, 0],
+      data: [0, 0, 60, 1,0],
       label: 'LS',
-      barPercentage: 0.60,
+      barPercentage: 0.70,
     }
   ];
-  public barChart2Labels: Array<any> = ['Anvers','Brugge', 'Lille','Barcelona', 'Paris'];
+  public barChart2Labels: Array<any> = ['Anvers','Lille', 'Brugge','Barcelona', 'Paris'];
   public barChart2Options: any = {
     /* tooltips: {
       enabled: false,
       custom: CustomTooltips
+    }, */
+    /* animation: {
+        onComplete: function () {
+            var chartInstance = this.chart,
+            ctx = chartInstance.ctx;
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'bottom';
+            this.data.datasets.forEach(function (dataset, i) {
+                var meta = chartInstance.controller.getDatasetMeta(i);
+                meta.data.forEach(function (bar, index) {
+                    var data = dataset.data[index];
+                    ctx.fillText(data, bar._model.x, bar._model.y + 5);
+                });
+            });
+        }
     }, */
     maintainAspectRatio: false,
     scales: {
@@ -107,12 +122,12 @@ export class TypographyComponent implements OnInit {
   };
   public barChart2Colours: Array<any> = [
     {
-      backgroundColor: 'rgba(255,0,0,.3)',
+      backgroundColor: 'rgba(255, 255, 255,.3)',
       borderWidth: 0
     }
   ];
-  public barChart2Legend = true;
-  public barChart2Type = 'line';
+  public barChart2Legend = false;
+  public barChart2Type = 'bar';
 
   // barChart3
   public barChart3Data: Array<any> = [
