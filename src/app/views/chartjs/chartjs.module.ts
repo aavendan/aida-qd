c5f1c9b40aa806b5e7cbb192d3c01312d2c30c47
aavendan <aavendan@gmail.com>
2021-05-25 15:41:24 -0500

    Revert "WidgetsComponent reference deleted"
    This reverts commit 34ee960bb605dbd3e94f9fa060b79c4adbad8bde.

--- src/app/views/chartjs/chartjs.module.ts
+++ src/app/views/chartjs/chartjs.module.ts
@@ -9,6 +9,6 @@ import { ChartJSRoutingModule } from './chartjs-routing.module';
     ChartJSRoutingModule,
     ChartsModule
   ],
-  declarations: [ ChartJSComponent ]
+  declarations: [ ChartJSComponent, WidgetsComponent ]
 })
 export class ChartJSModule { }
