f821c43007c70265e441270267aa96d9a8cc6809
aavendan <aavendan@gmail.com>
2021-05-25 15:27:35 -0500

    fail

--- src/app/views/chartjs/chartjs.module.ts
+++ src/app/views/chartjs/chartjs.module.ts
@@ -4,8 +4,6 @@ import { ChartsModule } from 'ng2-charts';
 import { ChartJSComponent } from './chartjs.component';
 import { ChartJSRoutingModule } from './chartjs-routing.module';
 
-import { WidgetsComponent } from './../widgets/widgets.component';
-
 @NgModule({
   imports: [
     ChartJSRoutingModule,
--- src/app/views/widgets/widgets.component.ts
+++ src/app/views/widgets/widgets.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { getStyle } from '@coreui/coreui/dist/js/coreui-utilities';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 
+
 @Component({
   templateUrl: 'widgets.component.html'
 })
